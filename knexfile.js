36bd4dfe09cd0c4954399859ad78a7f0a7d7f729
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-12-07 14:58:47 -0700

    adds underscore to database

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ export default {
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQLPORT || 3306,
   },
   pool: {
