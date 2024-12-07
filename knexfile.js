1fc4bb5c1efdaec536eeb561998016b5d1f6270b
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-12-07 14:23:29 -0700

    changes to railway env

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,18 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    port: process.env.MYSQLPORT,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./migrations",
   },
 };
